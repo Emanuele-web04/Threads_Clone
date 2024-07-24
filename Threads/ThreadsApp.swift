cf55c6b4aacaa41cb6ddb1300a6b1db5064aef8f
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-07-24 02:04:14 +0200

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Emanuele Di Pietro on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

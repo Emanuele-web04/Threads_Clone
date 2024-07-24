5b97532c71f6e1747df30e3e6b1ef5656a0b1989
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-07-24 02:10:57 +0200

    yes

--- Threads/ContentView.swift
+++ Threads/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, wor! hananasdsdfsd")
+            Text("So does it work?")
         }
         .padding()
     }
